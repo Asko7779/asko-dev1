a656058a857e3979a425b00801618a0f324fe84f
Asko7779 <stefanecadrian@gmail.com>
2024-08-18 01:31:02 +0200

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      base: 'src/main.jsx',
+      input: 'index.html',
+      external: ['react', 'react-dom'] // Ensure this is correct if you have a custom setup
+    },
+    },
 })
