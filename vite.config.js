81fc9f15c999d9c7e845739155e61dd80b83494b
Asko7779 <stefanecadrian@gmail.com>
2024-08-18 17:25:00 +0200

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -10,6 +10,7 @@ export default defineConfig({
     },
   },
   build: {
+    outDir: 'dist',
     rollupOptions: {
       external: ['./src/main.jsx'] // Ensure this is correct if you have a custom setup
     },
